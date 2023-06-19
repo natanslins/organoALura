bc9074033e604f7f3e1ae74a6f1becc7b2dc3f6c
Natan Szuvski Lins <108289477+natanslins@users.noreply.github.com>
2023-06-19 20:44:35 -0300

    Arquivos enviados para Github
    Versão 0.0

---
+++ src/componentes/CampoTexto/CampoTexto.js
@@ -0,0 +1,17 @@
+import './CampoTexto.css'
+
+export const CampoTexto = (props) => {
+ 
+  const placeHolderModificada = `${props.placeholder}...`
+
+  const aoDigitado = (evento) => {
+    props.aoAlterado(evento.target.value)
+  }
+
+  return (
+    <div className="campo-texto">
+      <label>{props.label}</label>
+      <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeHolderModificada}/>
+    </div>
+  )
+}
